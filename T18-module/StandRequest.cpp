3502f7ece9f1a8a6877a1d43db0d151d66f263dc
OstapenkoYaroslav <yarikostapenko122164@gmail.com>
2023-08-22 20:21:35 +0300

    Описан класс StandRequest

--- T18-module/StandRequest.cpp
+++ T18-module/StandRequest.cpp
@@ -1,1 +1,15 @@
 #include "StandRequest.h"
+
+void StandRequest::show() const
+{
+}
+
+string StandRequest::type() const
+{
+	return string();
+}
+
+float StandRequest::calcPrice() const
+{
+	return 0.0f;
+}
--- T18-module/StandRequest.h
+++ T18-module/StandRequest.h
@@ -2,5 +2,9 @@
 #include "Request.h"
 class StandRequest :public Request
 {
+public:
+	virtual void show() const = 0;
+	virtual string type()const = 0;
+	virtual float calcPrice() const = 0;
 };
 
