5e2370c2368fae294c9faec8e1cb3fe2d1339848
OstapenkoYaroslav <yarikostapenko122164@gmail.com>
2023-08-22 19:45:42 +0300

    Описал базовый класс Request

--- T18-module/Request.h
+++ T18-module/Request.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "Car.h"
+using namespace std;
+
 class Request
 {
+protected:
+	string name;
+	string number;
+	vector<Car> arr;
+public:
+	Request();
+	Request(string name,string number,Car obj);
+
+	string getName()const;
+	void setName(string name);
+
+	string getNumber()const;
+	void setNumber(string number);
+
+	vector<Car> getCarList();
+	void addCar(Car obj);
+	void delCar(int index);
+
+	virtual void show() const;
+	virtual string type()const;
+	virtual float calcPrice() const = 0;
 };
 
