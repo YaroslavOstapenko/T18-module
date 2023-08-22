ea8ed9c54e5f44c62881c52c8a54553117e3b7b1
OstapenkoYaroslav <yarikostapenko122164@gmail.com>
2023-08-22 21:29:27 +0300

    изменение StandRequest

--- T18-module/StandRequest.h
+++ T18-module/StandRequest.h
@@ -3,6 +3,7 @@
 class StandRequest :public Request
 {
 public:
+	StandRequest(string name, string number, Car* obj);
 	virtual void show() const override;
 	virtual string type()const override;
 	virtual float calcPrice() const override;
