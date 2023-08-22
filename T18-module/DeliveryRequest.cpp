b0e69d35cd246d7bbfb9694df87663948f95c6b9
OstapenkoYaroslav <yarikostapenko122164@gmail.com>
2023-08-22 21:35:31 +0300

    описание и реализация DeliveryRequest

--- T18-module/DeliveryRequest.cpp
+++ T18-module/DeliveryRequest.cpp
@@ -1,1 +1,35 @@
 #include "DeliveryRequest.h"
+
+DeliveryRequest::DeliveryRequest()
+{
+    percent = 3;
+}
+
+DeliveryRequest::DeliveryRequest(string name, string number, Car* obj, float percent)
+    :Request(name, number, obj),percent(percent)
+{
+}
+
+void DeliveryRequest::show() const
+{
+    cout << "ФИО" << name << endl;
+    cout << "Номер" << number << endl;
+    cout << "Процент" << percent << endl;
+    Request::getCarList();
+}
+
+string DeliveryRequest::type() const
+{
+    return "Заявка на отложенную поставку";
+}
+
+float DeliveryRequest::calcPrice() const
+{
+    float price;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        price = arr[i]->getPrice();
+        price - price * (percent / 100);
+    }
+    return price;
+}
