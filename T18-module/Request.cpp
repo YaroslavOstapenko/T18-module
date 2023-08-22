688e35e29fe8cf4aa070df0327ae6beb4bfa2541
OstapenkoYaroslav <yarikostapenko122164@gmail.com>
2023-08-22 20:04:15 +0300

    Изменено описание Request

--- T18-module/Request.cpp
+++ T18-module/Request.cpp
@@ -1,1 +1,54 @@
 #include "Request.h"
+
+Request::Request()
+{
+    name = "";
+    number = "";
+}
+
+Request::Request(string name, string number, Car obj)
+    :name(name),number(number)
+{
+    arr.push_back(obj);
+}
+
+string Request::getName() const
+{
+    return name;
+}
+
+void Request::setName(string name)
+{
+    if (name.length()>5)
+    {
+        this->name = name;
+    }
+}
+
+string Request::getNumber() const
+{
+    return number;
+}
+
+void Request::setNumber(string number)
+{
+    if (number.length()>0)
+    {
+        this->number = number;
+    }
+}
+
+vector<Car> Request::getCarList()
+{
+    return vector<Car>();
+}
+
+void Request::addCar(Car obj)
+{
+    arr.push_back(obj);
+}
+
+void Request::delCar(int index)
+{
+}
+
